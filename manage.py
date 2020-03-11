e534d62a83a2283f4fdec86acd89db5b47f143c0
clyde <clyde@opspl.com>
2020-03-11 18:29:59 +0530

    added register page

--- manage.py
+++ manage.py
@@ -5,6 +5,7 @@ from flask_cors import CORS
 from app.main import create_app
 from flask_script import Manager
 from flask_jwt_extended import JWTManager
+from flask import render_template
 
 app = create_app(os.getenv('ENV') or 'DEV')
 app.register_blueprint(blueprint)
@@ -12,6 +13,10 @@ app.app_context().push()
 jwt = JWTManager(app)
 manager = Manager(app)
 CORS(app)
+
+@app.route('/register/')
+def navigationMenu():
+   return render_template("register.html")
 
 if __name__ == '__main__':
     app.run()
